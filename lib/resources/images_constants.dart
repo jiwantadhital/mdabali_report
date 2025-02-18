63b5a769d8c1cee0589c0533ee7b81f7c5d6d799
@jiwanta.dhital023 <jiwantadhital@infodev.com.np>
2025-02-18 14:43:07 +0545

    images and nav

---
+++ lib/resources/images_constants.dart
@@ -0,0 +1,5 @@
+class ImagesConstants {
+  static const String info = 'assets/infologo.png';
+  static const String eye = 'assets/images/eye.png';
+  static const String eyeClose = 'assets/images/eye_close.png';
+}

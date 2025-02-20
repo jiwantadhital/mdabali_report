a2212408bab073c8c5f9b3eb83ef05df9416732c
vinay <vinaythapa762@gmail.com>
2025-02-20 17:50:00 +0545

    added validation

--- lib/view/login_page.dart
+++ lib/view/login_page.dart
@@ -10,6 +10,7 @@ import 'extracted_widgets/custom_text.dart';
 class LoginPage extends StatelessWidget {
   LoginPage({super.key});
   final _numberController = TextEditingController();
+   final _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
@@ -18,127 +19,139 @@ class LoginPage extends StatelessWidget {
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.all(20.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Container(
-                    width: 40,
-                    height: 40,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(20),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Container(
+                      width: 40,
+                      height: 40,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      child: Center(
+                          child: CustomText(
+                        text: 'Logo',
+                      )),
                     ),
-                    child: Center(
-                        child: CustomText(
-                      text: 'Logo',
-                    )),
-                  ),
-                  Row(
-                    children: [
-                      Container(
-                        padding: const EdgeInsets.symmetric(
-                          horizontal: 12,
-                          vertical: 6,
+                    Row(
+                      children: [
+                        Container(
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: 12,
+                            vertical: 6,
+                          ),
+                          decoration: BoxDecoration(
+                            color: Colors.grey[200],
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          child: Row(
+                            children: [
+                              Icon(Icons.language,
+                                  size: 16, color: Colors.grey[600]),
+                              const SizedBox(width: 4),
+                              CustomText(
+                                text: 'Eng',
+                                color: Colors.grey,
+                              )
+                            ],
+                          ),
                         ),
-                        decoration: BoxDecoration(
-                          color: Colors.grey[200],
-                          borderRadius: BorderRadius.circular(20),
+                        const SizedBox(width: 8),
+                        Container(
+                          padding: const EdgeInsets.all(8),
+                          decoration: BoxDecoration(
+                            color: Colors.grey[200],
+                            shape: BoxShape.circle,
+                          ),
+                          child: Icon(Icons.question_mark,
+                              size: 16, color: Colors.grey[600]),
                         ),
-                        child: Row(
-                          children: [
-                            Icon(Icons.language,
-                                size: 16, color: Colors.grey[600]),
-                            const SizedBox(width: 4),
-                            CustomText(
-                              text: 'Eng',
-                              color: Colors.grey,
-                            )
-                          ],
-                        ),
-                      ),
-                      const SizedBox(width: 8),
-                      Container(
-                        padding: const EdgeInsets.all(8),
-                        decoration: BoxDecoration(
-                          color: Colors.grey[200],
-                          shape: BoxShape.circle,
-                        ),
-                        child: Icon(Icons.question_mark,
-                            size: 16, color: Colors.grey[600]),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-              const SizedBox(height: 40),
-              // Title section
-              CustomText(
-                text: 'Secure and Convenient',
-                fontSize: 16,
-                color: Colors.grey,
-              ),
-              const SizedBox(height: 8),
-              CustomText(
-                text: 'Mobile Banking',
-                fontSize: 24,
-                color: Colors.deepOrange,
-                weight: FontWeight.bold,
-              ),
-              const SizedBox(height: 60),
-              CustomText(
-                text: 'Login or register',
-                fontSize: 24,
-                weight: FontWeight.bold,
-              ),
-
-              const SizedBox(height: 20),
-              CustomTextField(
-                hintText: 'Phone Number',
-                controller: _numberController,
-                contentPadding:
-                    EdgeInsets.symmetric(horizontal: 16, vertical: 16),
-                keyboardType: TextInputType.phone,
-              ),
-
-              const SizedBox(height: 60),
-
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  CustomText(
-                    text: 'Other Account Options',
-                    fontSize: 16,
-                    color: Colors.deepOrange,
-                  ),
-                  Icon(
-                    Icons.chevron_right,
-                    color: Colors.deepOrange,
-                  ),
-                ],
-              ),
-              const SizedBox(height: 34),
-
-              Center(
-                child: CustomText(
-                  text: 'Not this cooperative?',
-                  color: Colors.grey[600],
-                  decoration: TextDecoration.underline,
+                      ],
+                    ),
+                  ],
                 ),
-              ),
-              const Spacer(),
-
-              LoginButton(
-                
-                onPress: (){
-                  Get.to(PasswordLoginPage());
-                },
-                color: Colors.blue[100],
-                text: 'Continue',
-              ),
-              const SizedBox(height: 20),
-            ],
+                const SizedBox(height: 40),
+                // Title section
+                CustomText(
+                  text: 'Secure and Convenient',
+                  fontSize: 16,
+                  color: Colors.grey,
+                ),
+                const SizedBox(height: 8),
+                CustomText(
+                  text: 'Mobile Banking',
+                  fontSize: 24,
+                  color: Colors.deepOrange,
+                  weight: FontWeight.bold,
+                ),
+                const SizedBox(height: 60),
+                CustomText(
+                  text: 'Login or register',
+                  fontSize: 24,
+                  weight: FontWeight.bold,
+                ),
+            
+                const SizedBox(height: 20),
+                CustomTextField(
+                  hintText: 'Phone Number',
+                  controller: _numberController,
+                  contentPadding:
+                      EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+                  keyboardType: TextInputType.phone,
+                  validator:(value){
+                    if(value.isEmpty){
+                      return 'Required Phone Number';
+                    }
+                    else if(value.length<10){
+                      return 'Number should be of 10 digit';
+                    }
+                  }
+                ),
+            
+                const SizedBox(height: 60),
+            
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    CustomText(
+                      text: 'Other Account Options',
+                      fontSize: 16,
+                      color: Colors.deepOrange,
+                    ),
+                    Icon(
+                      Icons.chevron_right,
+                      color: Colors.deepOrange,
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 34),
+            
+                Center(
+                  child: CustomText(
+                    text: 'Not this cooperative?',
+                    color: Colors.grey[600],
+                    decoration: TextDecoration.underline,
+                  ),
+                ),
+                const Spacer(),
+            
+                LoginButton(
+                  
+                  onPress: (){
+                    if(_formKey.currentState?.validate()?? false){
+                    Get.to(PasswordLoginPage());}
+                  },
+                  color: Colors.blue[100],
+                  text: 'Continue',
+                ),
+                const SizedBox(height: 20),
+              ],
+            ),
           ),
         ),
       ),
--- lib/view/password_login_page.dart
+++ lib/view/password_login_page.dart
@@ -11,6 +11,7 @@ class PasswordLoginPage extends StatelessWidget {
 
   final _passwordController = TextEditingController();
   final _numberController =TextEditingController();
+  final _formKey = GlobalKey<FormState> ();
 
   @override
   Widget build(BuildContext context) {
@@ -19,177 +20,175 @@ class PasswordLoginPage extends StatelessWidget {
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.all(20.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Container(
-                    width: 40,
-                    height: 40,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(20),
-                    ),
-                    child:  Center(
-                      child: CustomText(
-                        text: 'Logo',
-                      )
-                    ),
-                  ),
-                  Row(
-                    children: [
-                      Container(
-                        padding: const EdgeInsets.symmetric(
-                          horizontal: 12,
-                          vertical: 6,
-                        ),
-                        decoration: BoxDecoration(
-                          color: Colors.grey[200],
-                          borderRadius: BorderRadius.circular(20),
-                        ),
-                        child: Row(
-                          children: [
-                            Icon(Icons.language, size: 16, color: Colors.grey[600]),
-                            const SizedBox(width: 4),
-                            CustomText(text: 'Eng',
-                            color: Colors.grey,)
-                          ],
-                        ),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Container(
+                      width: 40,
+                      height: 40,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
                       ),
-                      const SizedBox(width: 8),
-                      Container(
-                        padding: const EdgeInsets.all(8),
-                        decoration: BoxDecoration(
-                          color: Colors.grey[200],
-                          shape: BoxShape.circle,
-                        ),
-                        child: Icon(Icons.question_mark, size: 16, color: Colors.grey[600]),
+                      child:  Center(
+                        child: CustomText(
+                          text: 'Logo',
+                        )
                       ),
-                    ],
-                  ),
-                ],
-              ),
-              const SizedBox(height: 40),
-              // Title section
-              CustomText(
-                text: 'Secure and Convenient',
-                fontSize: 24,
-                color: Colors.grey,
+                    ),
+                    Row(
+                      children: [
+                        Container(
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: 12,
+                            vertical: 6,
+                          ),
+                          decoration: BoxDecoration(
+                            color: Colors.grey[200],
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          child: Row(
+                            children: [
+                              Icon(Icons.language, size: 16, color: Colors.grey[600]),
+                              const SizedBox(width: 4),
+                              CustomText(text: 'Eng',
+                              color: Colors.grey,)
+                            ],
+                          ),
+                        ),
+                        const SizedBox(width: 8),
+                        Container(
+                          padding: const EdgeInsets.all(8),
+                          decoration: BoxDecoration(
+                            color: Colors.grey[200],
+                            shape: BoxShape.circle,
+                          ),
+                          child: Icon(Icons.question_mark, size: 16, color: Colors.grey[600]),
+                        ),
+                      ],
+                    ),
+                  ],
                 ),
-              const SizedBox(height: 8),
-              CustomText(text:'Mobile Banking',
-              fontSize: 32,
-              color: Colors.deepOrange,
-              weight: FontWeight.bold,),
-              const SizedBox(height: 40),
-          
-          Container(
-            padding: EdgeInsets.symmetric(
-              horizontal: 16,
-              vertical: 16),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(30),
-                border: Border.all(
-                  color: Colors.grey[600]!
-                )
-              ),
-              child: Row(
-                children: [
-                  CircleAvatar(
-                    backgroundColor:Colors.grey[200],
-                    child: Icon(Icons.person,
-                    color: Colors.grey,),
+                const SizedBox(height: 40),
+                // Title section
+                CustomText(
+                  text: 'Secure and Convenient',
+                  fontSize: 24,
+                  color: Colors.grey,
+                  ),
+                const SizedBox(height: 8),
+                CustomText(text:'Mobile Banking',
+                fontSize: 32,
+                color: Colors.deepOrange,
+                weight: FontWeight.bold,),
+                const SizedBox(height: 40),
+                      UserPhoneNum(),
+                const SizedBox(height: 20),
+                CustomTextField(
+            
+                  hintText: 'Password',
+                   controller:_passwordController,
+                   validator: (value){
+                     if(value.isEmpty){
+                      return 'Required password';
+                     } 
+                                       },
+                   isPass: true,
+                   contentPadding: EdgeInsets.symmetric(
+                    horizontal: 16,
+                    vertical: 16),
+                    keyboardType: TextInputType.phone,
+                    suffixIconEnabled: true
+                    ),
+            
+                const SizedBox(height: 16),
+                
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    CustomText(text: 'Forgot Password?',
+                    fontSize: 16,
+                    color: Colors.black,
+                    decoration: TextDecoration.underline,),
                     
-                  ),
-                  const SizedBox(width: 12,),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      CustomText(text: 'Info Developers Pvt Ltd',
-                      weight: FontWeight.w400,
-                      color: Colors.black,),
-                      CustomText(text: '98*****93',
-                      color: Colors.grey[600],)
-                    ],
-                  )
-                ],
-              ),
-          ),
-              // CustomTextField(
-              //   maxLines: 2,
-              //   isLabel: false,
-              //   readOnly: true,
-              // //  contentPadding: EdgeInsets.all(12),
-              //   prefixIcon:Icon(Icons.person),
-              //   hintText:'Info Developers Pvt Ltd\n''984****93',
-              //   // Column(
-              //   //   children: [
-              //   //     CustomText(text: 'Info Devlopers Pvt Ltd',
-              //   //     color: Colors.black,
-              //   //     fontSize: 20,
-              //   //     weight: FontWeight.w400,),
-              //   //     const SizedBox(height: 5,),
-              //   //     CustomText(text: '984*****93',
-              //   //     color: Colors.grey,
-              //   //     fontSize: 15,
-              //   //     )
-              //   //   ],
-              //   // ),
-              //    controller:_numberController ),
-
-              const SizedBox(height: 20),
-              CustomTextField(
-
-                hintText: 'Password',
-                 controller:_passwordController,
-                 isPass: true,
-                 contentPadding: EdgeInsets.symmetric(
-                  horizontal: 16,
-                  vertical: 16),
-                  keyboardType: TextInputType.phone,
-                  suffixIconEnabled: true
-                  ),
-
-              const SizedBox(height: 16),
-              
-              Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  CustomText(text: 'Forgot Password?',
-                  fontSize: 16,
-                  color: Colors.black,
+                  ],
+                ),
+                const SizedBox(height: 16),
+                
+                Center(
+                  child:CustomText(text: 'Not you?',
+                  color: Colors.grey[600],
                   decoration: TextDecoration.underline,),
-                  
-                ],
-              ),
-              const SizedBox(height: 16),
-              
-              Center(
-                child:CustomText(text: 'Not you?',
-                color: Colors.grey[600],
-                decoration: TextDecoration.underline,),
-              ),
-              const Spacer(),
-              
-              LoginButton(
-                onPress: (){
-                  Get.to(PasswordLoginPage());
-                },
-                color: Colors.blue[100],
-                text: 'Login',
-                textcolor: Colors.black,
-                             
-              ),
-              const SizedBox(height: 20),
-                   
-             
-            ],
+                ),
+                const Spacer(),
+                
+                LoginButton(
+                  onPress: (){
+                   if(_formKey.currentState?.validate()??false){
+                     Get.to(PasswordLoginPage());
+                   }
+                  },
+                  color: Colors.blue[100],
+                  text: 'Login',
+                  textcolor: Colors.black,
+                               
+                ),
+                const SizedBox(height: 20),
+                     
+               
+              ],
+            ),
           ),
         ),
       ),
       bottomNavigationBar: BottomNavAuth(size: MediaQuery.of(context).size,)
+    );
+  }
+}
+
+class UserPhoneNum extends StatelessWidget {
+  const UserPhoneNum({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(
+        horizontal: 16,
+        vertical: 16),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(30),
+          border: Border.all(
+            color: Colors.grey[600]!
+          )
+        ),
+        child: Row(
+          children: [
+            CircleAvatar(
+              backgroundColor:Colors.grey[200],
+              child: Icon(Icons.person,
+              color: Colors.grey,),
+              
+            ),
+            const SizedBox(width: 12,),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                CustomText(text: 'Info Developers Pvt Ltd',
+                weight: FontWeight.w400,
+                color: Colors.black,),
+                CustomText(text: '98*****93',
+                color: Colors.grey[600],)
+              ],
+            )
+          ],
+        ),
     );
   }
 }

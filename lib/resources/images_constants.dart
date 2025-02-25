bfbdc9a457579c44a55e04a3f4aec419b5d51453
@jiwanta.dhital023 <jiwantadhital@infodev.com.np>
2025-02-25 09:53:15 +0545

    changes

--- lib/resources/images_constants.dart
+++ lib/resources/images_constants.dart
@@ -1,5 +1,5 @@
 class ImagesConstants {
-  static const String info = 'assets/infologo.png';
+  static const String info = 'assets/images/infologo.png';
   static const String eye = 'assets/images/eye.png';
   static const String eyeClose = 'assets/images/eye_close.png';
 }

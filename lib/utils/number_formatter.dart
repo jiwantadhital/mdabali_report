42b66030f987b464f4d78ae2ce43899c820bc738
aakashsahh <aakashsah181@gmail.com>
2025-03-30 17:47:15 +0545

    made function to add comma between number

---
+++ lib/utils/number_formatter.dart
@@ -0,0 +1,14 @@
+import 'package:intl/intl.dart';
+
+class NumberFormatter {
+  static String formatAmount(num? amount) {
+    if (amount == null) return "";
+
+    // Check if the number has decimal places
+    if (amount % 1 == 0) {
+      return NumberFormat("#,##0").format(amount); // No decimal places
+    } else {
+      return NumberFormat("#,##0.00").format(amount); // Two decimal places
+    }
+  }
+}

a4d7a37fc1552c2fb3c2199d385dae96d48e0dbc
aakashsahh <aakashsah181@gmail.com>
2025-03-30 17:44:43 +0545

    made monthly aggregate repo for api call

---
+++ lib/data/repos/repositories/monthly_aggreagate_repository.dart
@@ -0,0 +1,27 @@
+import 'dart:convert';
+import 'package:mdabali_report/data/models/monthly_aggregate_model.dart';
+import 'package:mdabali_report/data/repos/get_repo.dart';
+
+class MonthlyAggregateRepository {
+  final GetRepo getRepo;
+
+  MonthlyAggregateRepository({required this.getRepo});
+
+  Future<MonthlyAggregateModel> fetchMonthlyAggregate() async {
+    try {
+      final response = await getRepo.getRepository(
+        "/gateway/reportingApi/transaction/monthly-aggregate",
+      );
+
+      if (response.statusCode == 200) {
+        final data = jsonDecode(response.body);
+        return MonthlyAggregateModel.fromJson(data);
+      } else {
+        throw Exception(
+            jsonDecode(response.body)['message'] ?? "Failed to load data");
+      }
+    } catch (e) {
+      throw Exception("Error: $e");
+    }
+  }
+}

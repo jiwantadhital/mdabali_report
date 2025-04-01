405a409798936adae2f1959813a056c88ee5aba1
aakashsahh <aakashsah181@gmail.com>
2025-04-01 15:06:13 +0545

    made repository for past five month aggregate transaction

---
+++ lib/data/repos/repositories/five_month_data_repository.dart
@@ -0,0 +1,29 @@
+import 'dart:convert';
+import 'package:mdabali_report/data/models/five_month_data_model.dart';
+import 'package:mdabali_report/data/repos/get_repo.dart';
+import 'package:mdabali_report/resources/constants.dart';
+
+class FiveMonthDataRepository {
+  final GetRepo getRepo;
+
+  FiveMonthDataRepository({required this.getRepo});
+
+  Future<FiveMonthDataModel> fetchFiveMonthData(
+      {required String toDate}) async {
+    try {
+      final response = await getRepo.getRepository(
+        "${ApiClass.pastFiveMonthUrl}?toDate=$toDate",
+      );
+
+      if (response.statusCode == 200) {
+        final data = jsonDecode(response.body);
+        return FiveMonthDataModel.fromJson(data);
+      } else {
+        throw Exception(
+            jsonDecode(response.body)['message'] ?? "Failed to load data");
+      }
+    } catch (e) {
+      throw Exception("Error: $e");
+    }
+  }
+}

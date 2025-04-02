572b7d99ab4b19e5692a067d4f66e0936ffbfc53
aakashsahh <aakashsah181@gmail.com>
2025-04-02 15:42:35 +0545

    registered sms summary bloc

--- lib/main.dart
+++ lib/main.dart
@@ -5,12 +5,14 @@ import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:mdabali_report/bloc/five_month_data_bloc/bloc/five_month_data_bloc.dart';
 import 'package:mdabali_report/bloc/login_bloc/bloc/login_bloc.dart';
 import 'package:mdabali_report/bloc/monthly_aggregate_bloc/bloc/monthly_aggregate_bloc.dart';
+import 'package:mdabali_report/bloc/sms_summary_bloc/bloc/sms_summary_bloc.dart';
 import 'package:mdabali_report/bloc/summary_report_bloc/bloc/summary_report_bloc.dart';
 import 'package:mdabali_report/bloc/topup_summary_bloc/bloc/topup_summary_bloc.dart';
 import 'package:mdabali_report/data/repos/get_repo.dart';
 import 'package:mdabali_report/data/repos/repositories/five_month_data_repository.dart';
 import 'package:mdabali_report/data/repos/repositories/login_repository.dart';
 import 'package:mdabali_report/data/repos/repositories/monthly_aggreagate_repository.dart';
+import 'package:mdabali_report/data/repos/repositories/sms_summary_repository.dart';
 import 'package:mdabali_report/data/repos/repositories/summary_report_repository.dart';
 import 'package:mdabali_report/data/repos/repositories/topup_summary_repository.dart';
 import 'package:mdabali_report/data/shared_preferences/shared_preferences.dart';
@@ -48,6 +50,9 @@ class MyApp extends StatelessWidget {
             BlocProvider(
                 create: (context) => TopupSummaryBloc(
                     TopupSummaryRepository(getRepo: GetRepo()))),
+            BlocProvider(
+                create: (context) =>
+                    SmsSummaryBloc(SmsSummaryRepository(getRepo: GetRepo()))),
           ],
           child: GetMaterialApp(
               debugShowCheckedModeBanner: false,

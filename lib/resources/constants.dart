79c5ff59ff46e9de7a34441a1a8aa812f21d36c5
aakashsahh <aakashsah181@gmail.com>
2025-04-02 09:57:35 +0545

    added topup api url

--- lib/resources/constants.dart
+++ lib/resources/constants.dart
@@ -2,4 +2,6 @@ class ApiClass {
   static const String testUrl = "http://172.31.1.20";
   static const String pastFiveMonthUrl =
       '/gateway/reportingApi/transaction/past-five-month';
+  static const String topupUrl =
+      '/gateway/reportingApi/transaction/today-aggregate';
 }

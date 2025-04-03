3a995415498b70e64d124a504c4bfd865147ead8
aakashsahh <aakashsah181@gmail.com>
2025-04-03 10:14:58 +0545

    made shimmer effect for transaction card

---
+++ lib/view/transaction_page/shimmer_transaction_cards.dart
@@ -0,0 +1,190 @@
+// ignore_for_file: deprecated_member_use
+
+import 'package:flutter/material.dart';
+import 'package:shimmer/shimmer.dart';
+
+class ShimmerTransactionCard extends StatelessWidget {
+  const ShimmerTransactionCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    var colorScheme = Theme.of(context).colorScheme;
+    var brightness = Theme.of(context).brightness;
+    Color baseColor = brightness == Brightness.light
+        ? colorScheme.surfaceContainerHighest.withOpacity(0.5)
+        : colorScheme.surfaceContainerHighest.withOpacity(0.3);
+    Color highlightColor = brightness == Brightness.light
+        ? colorScheme.onSurface
+        : colorScheme.onSurface.withOpacity(0.6);
+
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 12),
+      child: Card(
+        elevation: 0,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: Container(
+          decoration: BoxDecoration(
+            color: colorScheme.surfaceContainerLowest,
+            borderRadius: BorderRadius.circular(16),
+            boxShadow: [
+              BoxShadow(
+                color: Color(0xFF4285F4).withOpacity(0.1),
+                offset: Offset(0, 4),
+                blurRadius: 12,
+                spreadRadius: 0,
+              ),
+            ],
+            border: Border.all(
+              color: colorScheme.primary.withOpacity(0.15),
+              width: 1.5,
+            ),
+          ),
+          padding: EdgeInsets.all(20),
+          child: Shimmer.fromColors(
+            baseColor: baseColor,
+            highlightColor: highlightColor,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      children: [
+                        Container(
+                          height: 38,
+                          width: 38,
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                        SizedBox(width: 12),
+                        Container(
+                          height: 20,
+                          width: 120,
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(4),
+                          ),
+                        ),
+                      ],
+                    ),
+                    Container(
+                      height: 30,
+                      width: 80,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 20),
+                Container(
+                  decoration: BoxDecoration(
+                    color: colorScheme.surfaceContainer,
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  padding: EdgeInsets.all(16),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      _buildShimmerStatColumn(),
+                      _buildVerticalDivider(context),
+                      _buildShimmerStatColumn(),
+                      _buildVerticalDivider(context),
+                      _buildShimmerStatColumn(),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildVerticalDivider(BuildContext context) {
+    return Container(
+      height: 50,
+      width: 1,
+      color: Colors.grey.withOpacity(0.2),
+    );
+  }
+
+  Widget _buildShimmerStatColumn() {
+    return Expanded(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          // Icon and label row
+          Row(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                height: 16,
+                width: 16,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  shape: BoxShape.circle,
+                ),
+              ),
+              SizedBox(width: 4),
+              Container(
+                height: 12,
+                width: 50,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(3),
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height: 10),
+          // Count value
+          Container(
+            height: 18,
+            width: 30,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(4),
+            ),
+          ),
+          SizedBox(height: 10),
+          // Amount value
+          Container(
+            height: 14,
+            width: 60,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(3),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ShimmerTransactionList extends StatelessWidget {
+  final int itemCount;
+
+  const ShimmerTransactionList({super.key, this.itemCount = 5});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      physics: NeverScrollableScrollPhysics(),
+      shrinkWrap: true,
+      itemCount: itemCount,
+      itemBuilder: (context, index) {
+        return ShimmerTransactionCard();
+      },
+    );
+  }
+}

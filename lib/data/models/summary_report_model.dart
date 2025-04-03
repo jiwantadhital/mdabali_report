81bc2e3f8218839ddd4890939cfb78f677ac0bd2
aakashsahh <aakashsah181@gmail.com>
2025-04-03 10:12:33 +0545

    made model for summary report

---
+++ lib/data/models/summary_report_model.dart
@@ -0,0 +1,80 @@
+class SummaryReportModel {
+  bool? status;
+  String? message;
+  List<Data>? data;
+  String? errors;
+  bool? genericMessage;
+
+  SummaryReportModel(
+      {this.status, this.message, this.data, this.errors, this.genericMessage});
+
+  SummaryReportModel.fromJson(Map<String, dynamic> json) {
+    status = json['status'];
+    message = json['message'];
+    if (json['data'] != null) {
+      data = <Data>[];
+      json['data'].forEach((v) {
+        data!.add(Data.fromJson(v));
+      });
+    }
+    errors = json['errors'];
+    genericMessage = json['genericMessage'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['status'] = status;
+    data['message'] = message;
+    if (this.data != null) {
+      data['data'] = this.data!.map((v) => v.toJson()).toList();
+    }
+    data['errors'] = errors;
+    data['genericMessage'] = genericMessage;
+    return data;
+  }
+}
+
+class Data {
+  double? successAmount;
+  int? pendingCount;
+  String? services;
+  String? serviceIcon;
+  double? failedAmount;
+  int? failedCount;
+  double? pendingAmount;
+  int? successCount;
+
+  Data(
+      {this.successAmount,
+      this.pendingCount,
+      this.services,
+      this.serviceIcon,
+      this.failedAmount,
+      this.failedCount,
+      this.pendingAmount,
+      this.successCount});
+
+  Data.fromJson(Map<String, dynamic> json) {
+    successAmount = json['successAmount'];
+    pendingCount = json['pendingCount'];
+    services = json['services'];
+    serviceIcon = json['serviceIcon'];
+    failedAmount = json['failedAmount'];
+    failedCount = json['failedCount'];
+    pendingAmount = json['pendingAmount'];
+    successCount = json['successCount'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['successAmount'] = successAmount;
+    data['pendingCount'] = pendingCount;
+    data['services'] = services;
+    data['serviceIcon'] = serviceIcon;
+    data['failedAmount'] = failedAmount;
+    data['failedCount'] = failedCount;
+    data['pendingAmount'] = pendingAmount;
+    data['successCount'] = successCount;
+    return data;
+  }
+}

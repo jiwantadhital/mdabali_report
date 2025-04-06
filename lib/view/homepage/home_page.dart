f1590ee2f84210af840dac87066a98d168c2b4ce
aakashsahh <aakashsah181@gmail.com>
2025-04-06 10:02:19 +0545

    added refresh indicator

--- lib/view/homepage/home_page.dart
+++ lib/view/homepage/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:mdabali_report/bloc/five_month_data_bloc/bloc/five_month_data_bloc.dart';
+import 'package:mdabali_report/bloc/monthly_aggregate_bloc/bloc/monthly_aggregate_bloc.dart';
 import 'package:mdabali_report/resources/images_constants.dart';
 
 import '../extracted_widgets/custom_text.dart';
@@ -14,89 +15,96 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var colorScheme = Theme.of(context).colorScheme;
-    return SingleChildScrollView(
-      child: Padding(
-        padding: EdgeInsets.all(16),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Center(
-                    child: Image.asset(
-                  ImagesConstants.arjnaLogo,
-                  height: 30,
-                  width: 30,
-                )),
-                const SizedBox(
-                  width: 5,
-                ),
-                CustomText(
-                  text: 'Arjan saving and credit cooperative',
-                  textOverflow: TextOverflow.ellipsis,
-                  letterSpacing: 1,
-                  fontSize: 16,
-                  weight: FontWeight.w600,
-                ),
-                const SizedBox(
-                  height: 24,
-                ),
-              ],
-            ),
-            const SizedBox(
-              height: 24,
-            ),
-            CustomText(
-              text: 'All your transaction details',
-              fontSize: 16,
-              weight: FontWeight.w600,
-              color: colorScheme.onSurface,
-            ),
-            const SizedBox(
-              height: 24,
-            ),
-            HeaderSection(),
-            const SizedBox(
-              height: 24,
-            ),
-            CustomText(
-              text: 'Summary of successful transactions',
-              fontSize: 16,
-              color: colorScheme.onSurface,
-              weight: FontWeight.w600,
-            ),
-            const SizedBox(
-              height: 24,
-            ),
-            PieChartCard(),
-            const SizedBox(
-              height: 24,
-            ),
-            CustomText(
-              text: 'Transaction Trends',
-              fontSize: 16,
-              weight: FontWeight.bold,
-            ),
-            const SizedBox(
-              height: 16,
-            ),
-            //line chart for Utility
-            UtilityLineChartSection(),
-            const SizedBox(
-              height: 24,
-            ),
-            //line chart for Dfs(Dr)
-            DfsDrLineChartSection(),
-            const SizedBox(
-              height: 24,
-            ),
-            //line chart for Dfs(cr   )
-            DfsCrLineChartSection(),
-            const SizedBox(
-              height: 24,
-            ),
-          ],
+
+    return RefreshIndicator(
+      onRefresh: () {
+        context.read<MonthlyAggregateBloc>().add(FetchMonthlyAggregate());
+        return Future.delayed(const Duration(milliseconds: 1200));
+      },
+      child: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Center(
+                      child: Image.asset(
+                    ImagesConstants.arjnaLogo,
+                    height: 30,
+                    width: 30,
+                  )),
+                  const SizedBox(
+                    width: 5,
+                  ),
+                  CustomText(
+                    text: 'Arjan saving and credit cooperative',
+                    textOverflow: TextOverflow.ellipsis,
+                    letterSpacing: 1,
+                    fontSize: 16,
+                    weight: FontWeight.w600,
+                  ),
+                  const SizedBox(
+                    height: 24,
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 24,
+              ),
+              CustomText(
+                text: 'All your transaction details',
+                fontSize: 16,
+                weight: FontWeight.w600,
+                color: colorScheme.onSurface,
+              ),
+              const SizedBox(
+                height: 24,
+              ),
+              HeaderSection(),
+              const SizedBox(
+                height: 24,
+              ),
+              CustomText(
+                text: 'Summary of successful transactions',
+                fontSize: 16,
+                color: colorScheme.onSurface,
+                weight: FontWeight.w600,
+              ),
+              const SizedBox(
+                height: 24,
+              ),
+              PieChartCard(),
+              const SizedBox(
+                height: 24,
+              ),
+              CustomText(
+                text: 'Transaction Trends',
+                fontSize: 16,
+                weight: FontWeight.bold,
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              //line chart for Utility
+              UtilityLineChartSection(),
+              const SizedBox(
+                height: 24,
+              ),
+              //line chart for Dfs(Dr)
+              DfsDrLineChartSection(),
+              const SizedBox(
+                height: 24,
+              ),
+              //line chart for Dfs(cr   )
+              DfsCrLineChartSection(),
+              const SizedBox(
+                height: 24,
+              ),
+            ],
+          ),
         ),
       ),
     );

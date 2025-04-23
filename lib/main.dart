7acceacc16b58c95703e10c46275732b1713df45
aakashsahh <aakashsah181@gmail.com>
2025-04-23 15:45:22 +0545

    intialized theme controller and added to get material app theme controller

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_sizer/flutter_sizer.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:mdabali_report/bloc/five_month_data_bloc/bloc/five_month_data_bloc.dart';
 import 'package:mdabali_report/bloc/login_bloc/bloc/login_bloc.dart';
@@ -10,6 +12,7 @@ import 'package:mdabali_report/bloc/sms_summary_bloc/bloc/sms_summary_bloc.dart'
 import 'package:mdabali_report/bloc/summary_report_bloc/bloc/summary_report_bloc.dart';
 import 'package:mdabali_report/bloc/topup_summary_bloc/bloc/topup_summary_bloc.dart';
 import 'package:mdabali_report/bloc/totp_bloc/bloc/t_otp_bloc.dart';
+import 'package:mdabali_report/controller/theme_controller.dart';
 import 'package:mdabali_report/data/repos/get_repo.dart';
 import 'package:mdabali_report/data/repos/repositories/five_month_data_repository.dart';
 import 'package:mdabali_report/data/repos/repositories/login_repository.dart';
@@ -21,19 +24,18 @@ import 'package:mdabali_report/data/repos/repositories/topup_summary_repository.
 import 'package:mdabali_report/data/repos/repositories/totp_repository.dart';
 import 'package:mdabali_report/data/shared_preferences/shared_preferences.dart';
 import 'package:mdabali_report/resources/colors.dart';
-import 'package:mdabali_report/view/dash_board_page.dart';
-import 'package:mdabali_report/view/o_t_p_verification_page.dart';
 import 'package:mdabali_report/view/password_login_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await UserSimplePreferences.init(); // Ensure SharedPreferences is initialized
-  runApp(const MyApp());
+  Get.put(ThemeController());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
+  MyApp({super.key});
+  final ThemeController themeController = Get.find();
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -67,19 +69,19 @@ class MyApp extends StatelessWidget {
           child: GetMaterialApp(
               debugShowCheckedModeBanner: false,
               title: 'Flutter Demo',
-              themeMode: ThemeMode.system,
-              darkTheme:ThemeData.dark(),
-              //  darkColorScheme.copyWith(
-              //   bottomNavigationBarTheme: const BottomNavigationBarThemeData(
-              //       backgroundColor: Color(0xFF000000)),
-              // ),
+              //themeMode: ThemeMode.system,
+              darkTheme:
+                  //    ThemeData.dark(),
+                  darkColorScheme.copyWith(
+                bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+                    backgroundColor: Color(0xFF000000)),
+              ),
               theme: lightColorScheme.copyWith(
                 bottomNavigationBarTheme: const BottomNavigationBarThemeData(
                   backgroundColor: Color(0xFFFFFFFF),
                 ),
               ),
-              home:DashBoardPage()),
-              // PasswordLoginPage()),
+              home: PasswordLoginPage()),
         );
       },
     );

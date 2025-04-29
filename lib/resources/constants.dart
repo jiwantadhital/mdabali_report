69eb07ca26d46231ce169afc1baaf2a7304ba2a8
aakashsahh <aakashsah181@gmail.com>
2025-04-29 14:50:42 +0545

    added init api url in constant

--- lib/resources/constants.dart
+++ lib/resources/constants.dart
@@ -7,4 +7,5 @@ class ApiClass {
       '/gateway/reportingApi/transaction/today-aggregate';
   static const String smsUrl = '/gateway/mdabaliApi/sms/aggregate-report';
   static const String memberLimitUrl = '/gateway/mdabaliApi/customer/limit';
+  static const String initUrl = '/gateway/mobileApi/user/init/reporting';
 }

8e89bad03095188f927fe30bea373843859c845d
aakashsahh <aakashsah181@gmail.com>
2025-04-30 12:31:51 +0545

    added method to fetch image using client id

--- lib/bloc/init_bloc/bloc/init_bloc.dart
+++ lib/bloc/init_bloc/bloc/init_bloc.dart
@@ -1,9 +1,12 @@
 import 'dart:async';
+import 'dart:typed_data';
 
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:http/http.dart' as http;
 import 'package:mdabali_report/data/models/init_model.dart';
 import 'package:mdabali_report/data/repos/repositories/init_repository.dart';
+import 'package:mdabali_report/resources/constants.dart';
 
 part 'init_event.dart';
 part 'init_state.dart';
@@ -18,9 +21,19 @@ class InitBloc extends Bloc<InitEvent, InitState> {
       FetchInitData event, Emitter<InitState> emit) async {
     emit(InitLoading());
     try {
+      // Fetch image using clientId
+
       final initModel = await initRepository.fetchInitData();
       if (initModel.status == true) {
-        emit(InitLoaded(initModel: initModel));
+        final imageUrl = Uri.parse(
+            '${ApiClass.testUrl}/gateway/webApi/client/image/${initModel.data?.clientId.toString()}');
+        final response = await http.get(imageUrl);
+
+        if (response.statusCode != 200) {
+          throw Exception("Failed to fetch image");
+        }
+        final imageBytes = response.bodyBytes;
+        emit(InitLoaded(initModel: initModel, imageBytes: imageBytes));
       } else {
         emit(InitFailure(error: initModel.message.toString()));
       }

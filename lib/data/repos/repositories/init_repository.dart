92dc542caa3ddfc922db150cab7bc801d908baad
aakashsahh <aakashsah181@gmail.com>
2025-05-06 14:52:04 +0545

    made method for client image url api call

--- lib/data/repos/repositories/init_repository.dart
+++ lib/data/repos/repositories/init_repository.dart
@@ -1,4 +1,6 @@
 import 'dart:convert';
+import 'dart:typed_data';
+import 'package:http/http.dart' as http;
 import 'package:mdabali_report/data/models/init_model.dart';
 import 'package:mdabali_report/data/repos/get_repo.dart';
 import 'package:mdabali_report/resources/constants.dart';
@@ -24,5 +26,16 @@ class InitRepository {
     } catch (e) {
       throw Exception("$e");
     }
+  }
+
+  Future<Uint8List> fetchImageUrl(String clientId) async {
+    final imageUrl =
+        Uri.parse('${ApiClass.testUrl}/gateway/webApi/client/image/$clientId');
+    final response = await http.get(imageUrl);
+    if (response.statusCode != 200) {
+      throw Exception("Failed to fetch image");
+    }
+    final imageBytes = response.bodyBytes;
+    return imageBytes;
   }
 }

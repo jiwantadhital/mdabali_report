23cf7b9a474344e11f35990e585df6edb73aa9e8
aakashsahh <aakashsah181@gmail.com>
2025-05-07 15:11:27 +0545

    made channel method to get android id

--- android/app/src/main/kotlin/com/example/mdabali_report/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/mdabali_report/MainActivity.kt
@@ -1,5 +1,29 @@
 package com.example.mdabali_report
-
+import android.os.Bundle
+import android.provider.Settings
+import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.plugin.common.MethodChannel
 
-class MainActivity: FlutterActivity()
+
+class MainActivity: FlutterActivity(){
+    private val CHANNEL_ANDROID_ID = "device_channel/android_id"
+
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        MethodChannel(
+            flutterEngine.dartExecutor.binaryMessenger,
+            CHANNEL_ANDROID_ID
+        ).setMethodCallHandler { call, result ->
+            if (call.method == "getAndroidId") {
+                val androidId = Settings.Secure.getString(
+                    contentResolver,
+                    Settings.Secure.ANDROID_ID
+                )
+                result.success(androidId)
+            } else {
+                result.notImplemented()
+            }
+        }
+    }
+}

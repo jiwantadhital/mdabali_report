27aeec5336a1233ae480f3f59175290cf626bb19
aakashsahh <aakashsah181@gmail.com>
2025-05-07 14:19:15 +0545

    sent device detail and menu type in the body

--- lib/data/repos/repositories/totp_repository.dart
+++ lib/data/repos/repositories/totp_repository.dart
@@ -1,17 +1,21 @@
 import 'dart:convert';
+import 'dart:io';
 import 'package:http/http.dart' as http;
 import 'package:mdabali_report/data/models/totp_model.dart';
 import 'package:mdabali_report/resources/constants.dart';
+import 'package:mdabali_report/utils/get_device_detail.dart';
 
 class TotpRepository {
   Future<TOtpModel> verifyOtp(String secret, String otp) async {
     try {
-      var uri = Uri.parse("${ApiClass.testUrl}/gateway/otp/verify/totp");
-
+      var uri = Uri.parse("${ApiClass.testUrl}${ApiClass.totpUrl}");
+      String menuType = Platform.isAndroid ? 'ANDROID' : 'IOS';
       var request = http.MultipartRequest('POST', uri);
       request.fields['secret'] = secret;
       request.fields['otp'] = otp;
-
+      request.fields['menu_type'] = menuType;
+      final deviceDetail = await getDeviceDetails();
+      request.fields['deviceDetail'] = jsonEncode(deviceDetail);
       var streamedResponse = await request.send();
       var response = await http.Response.fromStream(streamedResponse);
 
@@ -25,7 +29,7 @@ class TotpRepository {
             jsonDecode(response.body)['message'] ?? "Failed to verify OTP");
       }
     } catch (e) {
-      throw Exception("Otp error: ${e.toString()}");
+      throw Exception(e.toString());
     }
   }
 }

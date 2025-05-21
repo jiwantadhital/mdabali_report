c6dba5bff05aafba0aab88271a408846a86043ff
aakashsahh <aakashsah181@gmail.com>
2025-05-21 11:21:18 +0545

    formatted number in nepali number system

--- lib/utils/number_formatter.dart
+++ lib/utils/number_formatter.dart
@@ -6,9 +6,9 @@ class NumberFormatter {
 
     // Check if the number has decimal places
     if (amount % 1 == 0) {
-      return NumberFormat('#,##0').format(amount); // No decimal places
+      return NumberFormat('#,##,##0').format(amount); // No decimal places
     } else {
-      return NumberFormat('#,##0.00').format(amount); // Two decimal places
+      return NumberFormat('#,##,##0.00').format(amount); // Two decimal places
     }
   }
 }

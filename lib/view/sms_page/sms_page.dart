0a70bc03ba00dca2033e7e8076ce313ad0a9ea88
aakashsahh <aakashsah181@gmail.com>
2025-05-25 14:11:37 +0545

    made sms and topup element more elegant

--- lib/view/sms_page/sms_page.dart
+++ lib/view/sms_page/sms_page.dart
@@ -201,7 +201,6 @@ class _SmsPageState extends State<SmsPage> {
               borderRadius: BorderRadius.circular(24),
             ),
             child: ClipRRect(
-              borderRadius: BorderRadius.circular(24),
               child: BackdropFilter(
                 filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
                 child: Column(
@@ -246,30 +245,30 @@ class _SmsPageState extends State<SmsPage> {
           ),
 
           // Decorative bubble circle
-          Positioned(
-            top: 10,
-            right: -20,
-            child: Container(
-              width: 100,
-              height: 100,
-              decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                color: Colors.blue.withValues(alpha: 0.1),
-              ),
-            ),
-          ),
-          Positioned(
-            bottom: 20,
-            left: -30,
-            child: Container(
-              width: 80,
-              height: 80,
-              decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                color: Colors.blue.withValues(alpha: 0.1),
-              ),
-            ),
-          ),
+        //   Positioned(
+        //     top: 10,
+        //     right: -20,
+        //     child: Container(
+        //       width: 100,
+        //       height: 100,
+        //       decoration: BoxDecoration(
+        //         shape: BoxShape.circle,
+        //         color: Colors.blue.withValues(alpha: 0.1),
+        //       ),
+        //     ),
+        //   ),
+        //   Positioned(
+        //     bottom: 20,
+        //     left: -30,
+        //     child: Container(
+        //       width: 80,
+        //       height: 80,
+        //       decoration: BoxDecoration(
+        //         shape: BoxShape.circle,
+        //         color: Colors.blue.withValues(alpha: 0.1),
+        //       ),
+        //     ),
+        //   ),
         ],
       ),
     );
@@ -298,7 +297,7 @@ class _SmsPageState extends State<SmsPage> {
               borderRadius: BorderRadius.circular(20),
             ),
             child: ClipRRect(
-              borderRadius: BorderRadius.circular(24),
+              
               child: BackdropFilter(
                 filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
                 child: Column(
@@ -339,30 +338,30 @@ class _SmsPageState extends State<SmsPage> {
           ),
 
           // Decorative bubble circle
-          Positioned(
-            top: 10,
-            right: -20,
-            child: Container(
-              width: 100,
-              height: 100,
-              decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                color: Colors.blue.withValues(alpha: 0.1),
-              ),
-            ),
-          ),
-          Positioned(
-            bottom: 20,
-            left: -30,
-            child: Container(
-              width: 80,
-              height: 80,
-              decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                color: Colors.blue.withValues(alpha: 0.1),
-              ),
-            ),
-          ),
+        //   Positioned(
+        //     top: 10,
+        //     right: -20,
+        //     child: Container(
+        //       width: 100,
+        //       height: 100,
+        //       decoration: BoxDecoration(
+        //         shape: BoxShape.circle,
+        //         color: Colors.blue.withValues(alpha: 0.1),
+        //       ),
+        //     ),
+        //   ),
+        //   Positioned(
+        //     bottom: 20,
+        //     left: -30,
+        //     child: Container(
+        //       width: 80,
+        //       height: 80,
+        //       decoration: BoxDecoration(
+        //         shape: BoxShape.circle,
+        //         color: Colors.blue.withValues(alpha: 0.1),
+        //       ),
+        //     ),
+        //   ),
         ],
       ),
     );
